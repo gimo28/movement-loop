142a402bd7af4b0e01cb886277fe40e4a2d6dca2
gimo28 <113910413+gimo28@users.noreply.github.com>
2022-10-04 09:12:37 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 3; index++) {
+        for (let index = 0; index < 4; index++) {
+        	
+        }
+    }
+})
+let sprite = game.createSprite(4, -4)
+basic.forever(function () {
+	
+})
